5f761b2c1d05b69ccff83e5e4c474e6bef4f6563
service0427 <service0427@gmail.com>
2025-06-04 11:14:00 +0900

    Add sample Todo API

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const todosRouter = require('./routes/todos');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('✅ 서버 살아있음!');
 });
 
+app.use('/todos', todosRouter);
+
 app.listen(PORT, () => {
   console.log(`🚀 서버 실행 중: http://localhost:${PORT}`);
 });
-
-// 강제로 종료되는지 확인용 타이머 제거
